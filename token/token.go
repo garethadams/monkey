d11c8680bb876b8bc1a585496af516434787020f
Gareth Adams <100953050+garethadams@users.noreply.github.com>
2023-11-19 12:03:08 +0000

    Added basic lexer

--- token/token.go
+++ token/token.go
@@ -1,6 +1,8 @@
 package token
 
-type TokenType struct {
+type TokenType string
+
+type Token struct {
 	Type    TokenType
 	Literal string
 }
@@ -30,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
